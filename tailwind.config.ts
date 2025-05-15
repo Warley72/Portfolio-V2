84226e3c8f8f59515c24b1ca2444ee138efc919c
carlos warley <carloswarley0@gmail.com>
2025-05-15 20:16:13 -0300

    first commit

---
+++ tailwind.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./app/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        mono: ["var(--font-ibm)", "monospace"],
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
