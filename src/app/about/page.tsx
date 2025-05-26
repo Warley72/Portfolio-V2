598d439d5097d63f61ac45c0e2fc235b0cd68446
carlos warley <carloswarley0@gmail.com>
2025-05-26 00:32:41 -0300

    commitAbout

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -8,7 +8,7 @@ export default function About () {
             <Card className="p-6 bg-zinc-950 border border-white/20">
                 <h1 className="text-gray-500 text-3xl">Introduction</h1>
                 <h1 className="font-mono text-4xl  text-white">Front-end developer passionate about technology and coffee.</h1>
-                <p className="font-mono text-2xl  text-white">
+                <p className="font-mono text-2xl text-white">
                     I worked as a programming instructor at SuperGeeks and currently work as a freelancer, 
                     developing interfaces with React, Next.js, TypeScript, SASS and Lua, both for systems 
                     and windows at FiveM and I am looking to develop my skills eventually within the market.
@@ -20,6 +20,21 @@ export default function About () {
                     <span className="text-white">CSS</span>
                     <span className="text-white">TYPESCRIPT</span>
                     <span className="text-white">REACT</span>
+                </div>
+            </Card>
+
+            <Card className="p-6 bg-zinc-950 border border-white/20 mt-8">
+                <h1 className="font-mono text-2xl text-white">Career</h1>
+                <div>
+                    <div>
+                        <div className="flex items-center gap-x-2">
+                            <svg className="" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M16 20V4a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v16"></path><rect width="20" height="14" x="2" y="6" rx="2"></rect></svg>
+                            <span className="font-mono text-2xl text-white">Front-end Developer</span>
+                        </div>
+                        <span className="font-mono text-2xl text-white">Actio Software • Remote</span>
+                        <span className="font-mono text-2xl text-white">November 2023 – February 2025</span>
+                    </div>
+                    <div className="w-full h-px bg-zinc-800 opacity-50 my-4"/>
                 </div>
             </Card>
         </div>
